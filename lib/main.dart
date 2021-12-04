def2219add1d63b57392c3550986b567b8adadcb
loraien <loraien>
2021-12-04 08:50:08 +0530

    home appbar

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'error505',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("DASHBOARD",textAlign: TextAlign.center),
+          leading: IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.notes_outlined),
+            alignment: Alignment.topLeft,
+          ),
+          actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                Icons.notifications_on_outlined,
+                color: Colors.white),
+                alignment: Alignment.topRight,
+                onPressed: () {},
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
